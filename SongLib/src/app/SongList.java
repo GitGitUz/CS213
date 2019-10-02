1081cc194d067731103e54a9e90e5ab129dd8fe9
Visshal Suresh <skvisshal@gmail.com>
2019-10-02 02:08:05 -0400

    SongList Class added with add delete and edit functions

--- SongLib/src/app/SongList.java
+++ SongLib/src/app/SongList.java
@@ -4,7 +4,31 @@ import javafx.collections.ObservableList;
 
 public class SongList {
 	
-	public ObservableList<Song> songList;
+	private ObservableList<Song> songList;
+	
+	public boolean add(String name, String artist, String album, String year) {
+		if(name.isBlank()||artist.isBlank()) {return false;}
+		Song sng = new Song(name,artist,album,year);
+		for(Song s : songList) {
+			if(s.isDuplicate(sng))
+				return false;
+		}
+		return songList.add(sng);
+	}
+	
+	public boolean edit(Song sng, String name, String artist, String album, String year) {
+		songList.remove(sng);
+		if(!add(name,artist,album,year)) {
+			add(sng.getName(),sng.getArtist(),sng.getAlbum(),sng.getYear());
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean delete(Song sng) {
+		return songList.remove(sng);
+	}
+	
 	
 	
 }
